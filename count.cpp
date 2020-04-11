6ebd88c28d0f4f191e46dd129f1c875327b150f0
JanhaviAnap <63518181+JanhaviAnap@users.noreply.github.com>
2020-04-12 02:54:07 +0530

    Update count.cpp

--- count.cpp
+++ count.cpp
@@ -1,19 +1,28 @@
 #include<stdio.h>
-int num(int a[20])
+int num(char a[20])
 {
-	int count=0,i;
+	int count=0,i,t;
 	for(i=0;a[i]!='\0';i++)
-		count++;
+	{
+		t=a[i];
+		if(t>='0'&&t<='9')
+			count++;
+		else
+			return -1;
+	}
 	return count;
 }
 int main()
 {
-	int count,a1[20],a2[20],na1,na2;
+	char a1[20],a2[20];
+	int count,na1,na2;
 	printf("\Enter two numbers: \n");
 	scanf("%s %s",a1,a2);
 	na1=num(a1);
 	na2=num(a2);
-	if(na1==na2)
+	if(na1==-1||na2==-1)
+		printf("\nInvalid Input!");
+	else if(na1==na2)
 		printf("\nEqual");
 	else
 		printf("\nUnequal");
