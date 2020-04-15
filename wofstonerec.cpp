5f4736e7b656458eb9530c00b344ceac0021d0ec
JanhaviAnap <63518181+JanhaviAnap@users.noreply.github.com>
2020-04-15 23:54:16 +0530

    Add files via upload

---
+++ wofstonerec.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void arrange(int a[],int n)
+{
+	int i,j,t;
+	for(i=0;i<n;i++)//loop to arrange them in descending order
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(a[j]>=a[i])
+				{	
+					t=a[i];
+					a[i]=a[j];
+					a[j]=t;
+				}
+			}
+		}
+}
+int rec(int a[],int n)
+{
+	int i;
+	arrange(a,n);
+/*	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%3d",a[i]);
+	}*/
+	if(a[0]==a[1])
+		a[0]=0,a[1]=-1;
+	else
+		a[0]=a[0]-a[1],a[1]=-1;
+/*	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%3d",a[i]);
+	}*/
+	arrange(a,n);
+	if(a[1]==-1)
+		return a[0];
+	else
+		rec(a,n);
+}
+int main()
+{
+	int i,j,n,t,m1,m2;
+	printf("\nNumber of stones? ");
+	scanf("%d",&n);
+	int a[n];
+	printf("\nEnter weight of the stones ");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	printf("\nThe weight of stone is= %d",rec(a,n));
+	return 0;
+}
