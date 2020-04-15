996c05328aa3bb1bdd9c5bb88c0091c462d15d09
JanhaviAnap <63518181+JanhaviAnap@users.noreply.github.com>
2020-04-16 02:51:14 +0530

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -1,4 +1,13 @@
 #include<stdio.h>
+void check(int a[20],int n,int i,int t)
+{
+	int j;
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]+a[j]==t)
+			printf("\nSolution=[%d,%d]",i,j);	
+	} 
+}
 int main ()
 {
 	int a[20],n,i,j,t;
@@ -11,12 +20,6 @@ int main ()
 	scanf("%d",&t);
 	for(i=0;i<n;i++)
 	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]+a[j]==t)
-			{
-				printf("\nSolution=[%d,%d]",i,j);
-			}
-		}
+		check(a,n,i,t);
 	}
 }
