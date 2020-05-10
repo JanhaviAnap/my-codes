26a60b56e070902e004c82998234a945372aa613
JanhaviAnap <63518181+JanhaviAnap@users.noreply.github.com>
2020-05-11 00:00:33 +0530

    Update ptr6found.cpp

--- ptr6found.cpp
+++ ptr6found.cpp
@@ -10,7 +10,7 @@ struct node *start = NULL;
 int main(void) 
 {
     struct node *ptr,*newnode;
-    int val,a[5],i,num;
+    int c=0,a[20],i,num;
     printf("Enter 5 elements");
     for(i=0;i<5;i++)
         scanf("%d",&a[i]);
@@ -32,11 +32,31 @@ int main(void)
     printf("\n Enter the element to be searched");
     scanf("%d",&num);
     ptr=start;
+    i=0;
     while(ptr!=NULL) {
-        if(ptr->data==num)
-            printf("\nElement found!");
+        if(ptr->data==num){
+            printf("\nElement found at position %d !",i+1);
+            c++;
+        }
+        ptr=ptr->next;
+        i++;
+    }
+    if(c==0){
+        printf("\nElement not found");
+        a[i]=num;
+        newnode=malloc(sizeof(struct node));
+        newnode->data=num;
+        ptr=start;
+        while(ptr->next!=NULL)
+            ptr=ptr->next;
+        ptr->next=newnode;
+        newnode->next=NULL;
+        
+    }
+        ptr=start;
+    while(ptr!=NULL) {
+        printf("\n%d",ptr->data);
         ptr=ptr->next;
     }
-    
     return 0;
 }
